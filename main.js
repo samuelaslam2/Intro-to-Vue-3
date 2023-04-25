61e24b2e204c06fce4b4eada74754b051d11a03f
Samuel Aslam <samuel.aslam@alkami.com>
2023-04-25 16:57:06 -0500

    L8

--- main.js
+++ main.js
@@ -3,28 +3,39 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            brand: 'Vue Mastery',
+            selectedVariant:0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 },
+            ],
+            onSale: false
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    
+    computed: {
+        title() {
+            return  this.brand + ' ' + this.product; 
         },
-        // solution
-        updateImage(variantImage) {
-            this.image = variantImage
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale'
+            }
         }
     }
 })
