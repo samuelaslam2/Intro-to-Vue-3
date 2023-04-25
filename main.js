ae2947e764b96aecb87d613217870a824f1c7a1c
Samuel Aslam <samuel.aslam@alkami.com>
2023-04-25 14:09:19 -0500

    Reverting "L6-practice" to L6-start
    This reverts commit 342f9242ae4a0ffafb79c751c95e5df7e8ad8926.

--- main.js
+++ main.js
@@ -11,16 +11,5 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
-    },
-    methods: {
-        removeFromCart(){
-            this.cart -= 1
-        },
-        addToCart(){
-            this.cart += 1
-        },
-        changeImage(image){
-            this.image = image
-        }
     }
 })
